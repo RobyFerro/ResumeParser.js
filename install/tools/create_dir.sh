d21af7a9ccc044d72f929f99113d3eb78661ea7f
Roberto Ferro <roberto.ferro@ikdev.eu>
2019-06-28 12:46:05 +0200

    Fix path x2

--- install/tools/create_dir.sh
+++ install/tools/create_dir.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
 echo "CREATING TEMP AND RESULTS FOLDER"
-if [ ! -d "../../results" ]
+if [ ! -d "../results" ]
 then
-    mkdir -m777 ../../results
+    mkdir -m777 ../results
 else
     echo "- Results folder is already present"
 fi
 
-if [ ! -d "../../tmp" ]
+if [ ! -d "../tmp" ]
 then
-    mkdir -m777 ../../tmp
+    mkdir -m777 ../tmp
 else
     echo "- Temp folder is already present"
 fi
 
-if [ ! -d "../../tmp/document" ]
+if [ ! -d "../tmp/document" ]
 then
-    mkdir -m777 ../../tmp/document
+    mkdir -m777 ../tmp/document
 else
     echo "- Document folder is already present"
 fi
 
-if [ ! -d "../../tmp/images" ]
+if [ ! -d "../tmp/images" ]
 then
-    mkdir -m777 ../../tmp/images
+    mkdir -m777 ../tmp/images
 else
     echo "- Images folder is already present"
 fi
