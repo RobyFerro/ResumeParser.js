c68e27ca66b4fcdec6bbfbf29218885712f95ce8
Roberto Ferro <roberto.ferro@ikdev.eu>
2019-06-28 12:19:12 +0200

    Fix debian install file x3

--- install/debian.sh
+++ install/debian.sh
@@ -9,5 +9,5 @@ sudo apt-get -y install antiword
 sudo apt-get -y install unrtf
 sudo apt-get -y install tesseract-ocr
 
-chmod +x ./tools/python_dependencies.sh && ./tool/python_dependencies.sh
-chmod +x ./tools/create_dir.sh && ./tool/create_dir.sh
+chmod +x ./tools/python_dependencies.sh && ./tools/python_dependencies.sh
+chmod +x ./tools/create_dir.sh && ./tools/create_dir.sh
