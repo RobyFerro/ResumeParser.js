e862e47c9289b0ab0b752d217aaba2eaea02cc12
Roberto Ferro <roberto.ferro@ikdev.eu>
2019-06-28 12:16:55 +0200

    Fix debian install file x2

--- install/debian.sh
+++ install/debian.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+sudo apt-get update
 sudo apt-get -y install cmake
 sudo apt-get -y install libopenblas-dev
 sudo apt-get -y install libx11-dev
@@ -8,5 +9,5 @@ sudo apt-get -y install antiword
 sudo apt-get -y install unrtf
 sudo apt-get -y install tesseract-ocr
 
-chmod +x ./tools/python_dependencies.sh && bin/python_dependencies.sh
-chmod +x ./tools/create_dir.sh && bin/create_dir.sh
+chmod +x ./tools/python_dependencies.sh && ./tool/python_dependencies.sh
+chmod +x ./tools/create_dir.sh && ./tool/create_dir.sh
