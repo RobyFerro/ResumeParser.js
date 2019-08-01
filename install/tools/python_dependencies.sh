f69863b32c460a1e6d185ae3496eae28f59a6ad7
Roberto Ferro <roberto.ferro@ikdev.eu>
2019-08-01 12:49:52 +0200

    Insert --user argument

--- install/tools/python_dependencies.sh
+++ install/tools/python_dependencies.sh
@@ -4,5 +4,5 @@ if pip list 2>&1 /dev/null | grep -q "face-recognition"
 then
     echo "- Face recognition is already installed"
 else
-   pip install face_recognition
+   pip install --user face_recognition
 fi
