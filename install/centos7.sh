e90f1da95be3e724fcfd71d416803c4cd518da9f
Roberto Ferro <roberto.ferro@ikdev.eu>
2019-08-06 12:40:11 +0200

    Update centos7.sh

--- install/centos7.sh
+++ install/centos7.sh
@@ -2,18 +2,13 @@
 sudo yum-config-manager --add-repo https://download.opensuse.org/repositories/home:/Alexander_Pozdnyakov/CentOS_7/
 sudo rpm --import https://build.opensuse.org/projects/home:Alexander_Pozdnyakov/public_key
 yum check-update
-sudo yum install -y cmake
-sudo yum install -y libX11-devel.x86_64
-sudo yum install -y libpng-devel
-sudo yum install -y poppler-utils
-sudo wget https://forensics.cert.org/cert-forensics-tools-release-el7.rpm
-sudo rpm -Uvh cert-forensics-tools-release*rpm
-sudo yum --enablerepo=forensics install -y antiword
-sudo wget http://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/u/unrtf-0.21.9-8.el7.x86_64.rpm
-sudo rpm -Uvh unrtf-0.21.9-8.el7.x86_64.rpm
-sudo yum install unrtf
-sudo yum install -y tesseract
-sudo yum install -y python-devel
+yum -y install cmake libX11-devel.x86_64 libpng-devel poppler-utils
+yum -y install https://forensics.cert.org/cert-forensics-tools-release-el7.rpm
+yum --enablerepo=forensics install -y antiword
+yum -y install http://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/u/unrtf-0.21.9-8.el7.x86_64.rpm
+yum -y install unrtf tesseract python-devel python2-pip
+yum -y install libreoffice
+pip -y install --upgrade pip
 sudo rm -rf *.rpm
 
 sudo chmod +x ./tools/python_dependencies.sh && ./tools/python_dependencies.sh
